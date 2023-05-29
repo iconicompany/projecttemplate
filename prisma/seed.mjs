8724222a707382d5b0ca28877af3be1e86e58e6d
Vjacheslav Borisov <slavb@iconicompany.com>
2023-05-29 11:17:48 +0400

    fix migrate

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -1,4 +1,4 @@
-import prisma from '@/libs/prisma.mjs';
+import prisma from '../src/libs/prisma.mjs';
 import BusinessEntitySeeder from './seeds/BusinessEntitySeeder.mjs';
 import PermissionSeeder from './seeds/PermissionSeeder.mjs';
 import RoleSeeder from './seeds/RoleSeeder.mjs';
